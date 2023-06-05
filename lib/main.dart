800bd1df7caa8054b07173db16ff704785e843e9
Farooq2002 <fa552991@gmail.com>
2023-06-06 03:23:09 +0500

    Figma File To Flutter

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/sound-bar-app-ui/.dart';
+
+import '../../../../StudioProjects/maveshi_new/lib/utils.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter',
+      debugShowCheckedModeBanner: false,
+      scrollBehavior: MyCustomScrollBehavior(),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        body: SingleChildScrollView(
+          child: Scene(),
+        ),
+      ),
+    );
+  }
+}
